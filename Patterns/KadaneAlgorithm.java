6dd1afaf7a0c14660c45f9f2b5ce1d00ed4a656a
Vooka Kavya Suma <vookakavyasuma4@gmail.com>
2026-01-03 22:32:27 +0530

    Implemented Kadane's Algorithm for maximum subarray

---
+++ Patterns/KadaneAlgorithm.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class KadaneAlgorithm {
+    public static int KadaneAlg(int[] arr) {
+        int maxSum = 0, currentSum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] + currentSum > 0) {
+                currentSum += arr[i];
+                maxSum = currentSum;
+            } else {
+                currentSum = 0;
+            }
+        }
+        return Math.max(currentSum, maxSum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the count of values in the array :  ");
+        int n = sc.nextInt();
+        System.out.print("Enter the values into array :  ");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = KadaneAlg(arr);
+        System.out.println("The maximun sum is " + result);
+        sc.close();
+    }
+}
